4f823dc9962c659492da340de83c113fc94021c0
李青 <liqingitt@gmail.com>
2023-07-09 14:28:58 +0800

    feat: 实现jsx函数 与 打包

--- packages/index.ts
+++
@@ -1,5 +0,0 @@
-const a = 1;
-const b = 2;
-if (a === 2) {
-	const bbb = 234;
-}
---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,48 @@
+import { getBaseRollupPlugins, getPackageJson, resolvePkgPath } from './utils';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+const { name, module } = getPackageJson('react');
+
+const pkgPath = resolvePkgPath(name);
+
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index.js`,
+			name,
+			format: 'umd'
+		},
+		plugins: [
+			...getBaseRollupPlugins(),
+			generatePackageJson({
+				input: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, description, version }) => ({
+					name,
+					description,
+					version,
+					main: 'index.js'
+				})
+			})
+		]
+	},
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			// jsx/runtime
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime',
+				format: 'umd'
+			},
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime',
+				format: 'umd'
+			}
+		],
+		plugins: getBaseRollupPlugins()
+	}
+];
